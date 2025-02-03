df820b4da7a85f73ca7e8ebfb20291284fbc1609
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 00:10:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ class Person {
 		this._name = name;
 		this._age = age;
 	}
-	get name() return this._name;
-	get age() return this._age;
-	set age(value) this._age = value;
+	get name() {return this._name;}
+	get age() {return this._age;}
+	set age(value) {this._age = value;}
 }
 
 class Student extends Person {
 	study(){
-		console.log(`${name} is studying`);
+		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${name} is teaching`);
+		console.log(`${this.name} is teaching`);
 	}
 }
 
